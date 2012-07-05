08c4af755f65a723a9e6a3206c83d207aad58cca
spukyn <spukyn@spukyn-Vostro-1520.(none)>
2012-07-05 19:23:31 +0200

    conflicto

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <html>
 	<head>
 		<title>Prueba</title>
+		
 	</head>
 	<body >	
 		<form action="login.php" method="post">
-			Identificacion<br/>
+			<div>Identificación<div/><br/>
 			
 			<center>	
 				<div><b>Nombre de usuario</b></div>
-				<INPUT type=text size=10 name="usuario">
+				<div><INPUT type="text" size=10 name="usuario"></div>
 					
 				<div><b>Password</b></div>
-				<INPUT type=password size=10 name="password"><br/>
+				<INPUT type="password" size=10 name="password"><br/>
 					
 				<INPUT type="submit" value="Enviar">	
 			</center>
