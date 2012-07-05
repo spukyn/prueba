b4d383d5e965bbcf798bb92ab503d28fdc8f382b
Evan <evan@localhost.localdomain>
2012-07-05 23:58:21 +0200

    centrado

--- index.php
+++ index.php
@@ -5,26 +5,27 @@
 			body {
     		color: purple;
     		text-align: center;
-    		vertical-align: middle;
     		background-color: #d8da3d }	
-    					
+		
 		</style>
 		
 		
 	</head>
 	<body> 
 		<form action="login.php" method="post" >
+			<div style="position: absolute;left: 50%;top: 50%">
 				<div >Nombre de usuario</div>
 				<div><INPUT type="text" size=10 name="usuario"></div>
 					
 				<div>Password</div>
 				<div><INPUT type="password" size=10 name="password"></div>
 					
-				<div><INPUT type="submit" value="Enviar" onclick="alert('caca')"></div>			
+				<div><INPUT type="submit" value="Enviar" onclick="alert('caca')"></div>
+			</div>			
 		</form>	
-		<form action="subirarchivo.php" method="post">
+		<!-- <form action="subirarchivo.php" method="post">
 			<div><input type="file" name="archivo"></div>
 			<div><INPUT type="submit" value="Subir archivo"></div>			
-		</form>
+		</form> -->
 	</body>
 </html>
