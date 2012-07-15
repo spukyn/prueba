8f9b06fc177a5b9db299174666269c7f219a1faa
spukyn <spukyn@spukyn-Vostro-1520.(none)>
2012-07-15 23:07:19 +0200

    version dos de hola mundo con ajax hecha con un js aparte.

---
+++ ajaxv2.js
@@ -0,0 +1,41 @@
+	var READY_STATE_UNINITIALIZED=0;
+		var READY_STATE_LOADING=1;
+		var READY_STATE_LOADED=2;
+		var READY_STATE_INTERACTIVE=3;
+		var READY_STATE_COMPLETE=4;
+
+		var peticion_http;
+		
+		function cargarContenido(url,metodo,funcion){
+			
+			peticion_http = inicializaXHR();
+			
+			if(peticion_http){
+				peticion_http.onreadystatechange=funcion;
+				peticion_http.open(metodo,url,true);
+				peticion_http.send(null);
+			}
+		}
+		
+		function inicializaXHR(){
+			if(window.XMLHttpRequest){
+				return new XMLHttpRequest();
+			}
+			else if(window.ActiveXObject){
+				return new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		}
+		
+		function muestraContenido(){
+			if(peticion_http.readyState==READY_STATE_COMPLETE){
+				if(peticion_http.status==200){
+					alert(peticion_http.responseText);
+				}
+			}
+		}
+		
+		function descargaArchivo(){
+			cargarContenido("http://localhost/Prueba/holamundo.txt","GET",muestraContenido);
+		}
+		
+		window.onload = descargaArchivo;
--- hmajax2.php
+++ hmajax2.php
@@ -1,49 +1,8 @@
 <html>
 	<head>
 		<title>Hola mundo ajaxv2</title>
-		<script type="text/javascript">
-		
-		//Estados de las respuestas del server
-		var READY_STATE_UNINITIALIZED=0;
-		var READY_STATE_LOADING=1;
-		var READY_STATE_LOADED=2;
-		var READY_STATE_INTERACTIVE=3;
-		var READY_STATE_COMPLETE=4;
-
-		var peticion_http;
-		
-		function cargarContenido(url,metodo,funcion){
-			peticion_http = inicializaXHR();
+		<script type="text/javascript" src="ajaxv2.js">
 			
-			if(peticion_http){
-				peticion_http.onreadyStatechange=funcion;
-				peticion_http.open(metodo,url,true);
-				peticion_http.send(null);
-			}
-		}
-		
-		function inicializaXHR(){
-			if(window.XMLHttpRequest)
-				return new XMLHttpRequest();
-			}
-			else if(window.ActiveXObject){
-				return new ActiveXObject("Microsoft.XMLHTTP");
-			}
-		}
-		
-		function muestraContenido(){
-			if(peticion_http.readyState==READY_STATE_COMPLETE){
-				if(peticion_http.status==200){
-					alert(peticion_http.responseText);
-				}
-			}
-		}
-		
-		function descargaArchivo(){
-			cargarContenido("http://localhost/Prueba/holamundo.txt","GET",muestraContenido);
-		}
-		
-		window.onload= descargaArchivo;
 		
 		
 		</script>
