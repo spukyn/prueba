dfa3f47559041b29b3e3580a2c3c2cb4bf002671
spukyn <spukyn@spukyn-Vostro-1520.(none)>
2012-07-15 18:57:17 +0200

    hola mundo no funciona

---
+++ hmajax.php
@@ -0,0 +1,38 @@
+
+<html>
+	<head>
+		<title>Hola Mundo Ajax</title>
+		
+		<script type="text/javascript">
+			Function descargaArchivo(){
+				if(window.XMLHttpRequest){
+					peticion_http = new XMLHttpRequest();
+				}
+				else if(window.ActiveXObject){
+					peticion_http = new  ActiveXObject("Microsoft.XMLHTTP");
+				}
+				
+				peticion_http.onreadystatechange = muestraContenido;
+				
+				peticion_http.open('GET','http://localhost/Prueba/holamundo.txt',true);
+				peticion_http.send(null);	
+				
+				Function muestraContenido(){
+					if(peticion_http.readyState == 4){
+						alert("hola holaaaaa");
+						if(peticion_http.status == 200){
+							alert(peticion_http.responseText);
+						}
+					}
+				}	
+				
+			}
+			
+			window.onload = descargaArchivo;
+			
+		</script>
+	</head>
+	<body>
+		
+	</body>
+</html>
