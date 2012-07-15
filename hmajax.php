c6d84dc1a7f1f626d16bdb97402a92ada7ebd611
Evan <evan@localhost.localdomain>
2012-07-15 19:03:37 +0200

    error caca

--- hmajax.php
+++ hmajax.php
@@ -4,7 +4,7 @@
 		<title>Hola Mundo Ajax</title>
 		
 		<script type="text/javascript">
-			Function descargaArchivo(){
+			function descargaArchivo(){
 				if(window.XMLHttpRequest){
 					peticion_http = new XMLHttpRequest();
 				}
@@ -17,7 +17,7 @@
 				peticion_http.open('GET','http://localhost/Prueba/holamundo.txt',true);
 				peticion_http.send(null);	
 				
-				Function muestraContenido(){
+				function muestraContenido(){
 					if(peticion_http.readyState == 4){
 						alert("hola holaaaaa");
 						if(peticion_http.status == 200){
