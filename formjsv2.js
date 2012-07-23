886caa0f1a9ebff6941c70ac799284621bf45085
spukyn <spukyn@spukyn-Vostro-1520.(none)>
2012-07-23 20:04:47 +0200

    formulario V2

---
+++ formjsv2.js
@@ -0,0 +1,60 @@
+/**
+ * @author spukyn
+ */
+
+var READY_STATE_COMPLETE=4;
+var peticion_http=null;
+
+function inicializaXHR(){
+	if(window.XMLHttpRequest){
+		return new XMLHttpRequest();
+	}
+	else if(window.ActiveXObject){
+		return new ActiveXObject("Microsoft.XMLHTTP");
+	}
+}
+
+function crea_query_string(){
+	var fechaN = document.getElementById("fecha_nacimiento");
+	var codigopostal = document.getElementById("codigo_postal");
+	var telf = document.getElementById("telefono");
+	
+	return "fecha_nacimiento=" + encodeURIComponent(fechaN.value) +
+			"&codigo_postal=" + encodeURIComponent(codigopostal.value) +
+			"&telefono=" + encodeURIComponent(telf.value) +
+			"&nocache=" + Math.random();
+}
+
+function valida(){
+	peticion_http = inicializaXHR();
+	
+	if(peticion_http){
+		peticion_http.onreadystatechange = procesaRespuesta;
+		peticion_http.open("POST","http://localhost/Prueba/validadatosv2.php",true);
+		peticion_http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		
+		var query_string = crea_query_string();
+		peticion_http.send(query_string);
+	}
+}
+
+function procesaRespuesta(){
+	if(peticion_http.readyState == READY_STATE_COMPLETE){
+		if(peticion_http.status== 200){
+			var respuesta_json = peticion_http.responseText;
+			var objeto_json = eval("("+respuesta_json+")");
+			var objeto_json = eval("("+respuesta_json+")");
+
+			
+			
+			document.getElementById("respuesta").innerHTML= objeto_json.mensaje;
+			document.getElementById("fecha_nacimiento").value=""; //no se si lo de value es la forma mas adecuada de hacer esto..
+			document.getElementById("codigo_postal").value="";
+			document.getElementById("telefono").value="";
+			}
+			if(!peticion_http.responseText){
+				alert("Registro completo");
+				
+		}
+	}
+}
---
+++ formulariov2.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<script type="text/javascript" src="formjsv2.js"></script>
+		
+	</head>
+	<body>
+		<form>
+			<label for="fecha_nacimiento">Fecha de nacimiento:</label>
+			<input type="text" id="fecha_nacimiento" name="fecha nacimiento"/><br />
+		
+			<label for="codigo_postal">Codigo postal:</label>
+			<input type="text" id="codigo_postal" name="codigo_postal"/><br />
+			
+			<label for="telefono">Telefono:</label>
+			<input type="text" id="telefono" name="telefono"/><br />
+			
+			<input type="button" value="Validar" onclick="valida()" />			
+		</form>
+		
+		<div id="respuesta"></div>
+	</body>
+</html>
